eb2af681540596807e1179e90a14d5bf5ef3de4f
kaylie weable <kayliew@auctionedge.com>
2017-07-07 10:23:57 -0700

    action creator returns object with type

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
 export function selectBook(book) {
-  console.log('book selected: ', book.title);
+  //selectBook is an action creator and needs to return an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
